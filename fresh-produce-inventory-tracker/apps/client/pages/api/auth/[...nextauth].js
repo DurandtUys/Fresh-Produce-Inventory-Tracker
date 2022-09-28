1d4910c6a752b5965b2de5b5fa9e6008ff65df06
Durandt Uys <u18286250@tuks.co.za>
2022-09-28 19:58:24 +0200

    update

--- fresh-produce-inventory-tracker/apps/client/pages/api/auth/[...nextauth].js
+++ fresh-produce-inventory-tracker/apps/client/pages/api/auth/[...nextauth].js
@@ -27,8 +27,8 @@ const providers = [
         throw new Error(errorMessage);
       }
     },
-    secret: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET:process.env.NEXT_PUBLIC_SECRET,
+    secrete: process.env.NEXTAUTH_URL,
+    secret:"755f1bd1cc5cfc20ab0931f9410bb83d",
   }),
 ];
 
